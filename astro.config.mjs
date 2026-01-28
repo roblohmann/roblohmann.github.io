8eb848fe4d8402cccbd011ca5f216578d4d088c5
Rob Lohmann <roblohman@gmail.com>
2026-01-28 09:53:28 +0100

    add github action for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
+import github from '@astrojs/github-pages';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://roblohmann.github.io',
-  integrations: [mdx(), tailwind()],
+  integrations: [mdx(), tailwind(), github()],
+  output: 'static',
+  adapter: github(),
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
