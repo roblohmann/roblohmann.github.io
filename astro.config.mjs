a8d14597cc6eba2b272a140b798821312810dd63
Rob Lohmann <roblohman@gmail.com>
2026-01-28 10:11:21 +0100

    remove github page adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
-import github from '@astrojs/github-pages';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://roblohmann.github.io',
   base: '/', 
-  integrations: [mdx(), tailwind(), github()],
+  integrations: [mdx(), tailwind()],
   output: 'static',
-  adapter: github(),
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
